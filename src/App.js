a2c74f73b2fc58195e565ed47953cfb5a5171386
danielhumgon <danielhumgon@gmail.com>
2018-12-25 12:01:03 +0200

    added new routes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
 import React, { Component } from 'react';
-import { Link, BrowserRouter as Router } from 'react-router-dom';
+import { Link, BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.css';
 
 class App extends Component {
+  
+  renderHome = () => <h1>Home</h1>;
+
+  renderCustomerContainer = () => <h1>Customer Container</h1>;
+
+  renderCustomerListContainer = () => <h1>Customers List Container</h1>;
+
+  renderCustomerNewContainer = () => <h1>Customer New Container</h1>;
+
+
   render() {
     return (
       <Router>
-      <div className="App">
-          <Link to="/customers">Customers</Link>
+      <div>
+          <Route exact path="/" component={this.renderHome}/>
+          <Route exact path="/customers" component={this.renderCustomerListContainer}/>
+          <Route exact path="/customers/:dni" component={this.renderCustomerContainer}/>
+          <Route exact path="/customers/new" component={this.renderCustomerNewContainer}/>
       </div>
       </Router>
     );
