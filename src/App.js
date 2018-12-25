deb6c27ed8e0732db6d66bddf839a580f1e54c09
danielhumgon <danielhumgon@gmail.com>
2018-12-25 08:46:17 +0200

    added customer list item component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
+import { Link, BrowserRouter as Router } from 'react-router-dom';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
+      <Router>
       <div className="App">
-        
+          <Link to="/customers">Customers</Link>
       </div>
+      </Router>
     );
   }
 }
