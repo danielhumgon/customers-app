5e3887339e2da76e3af25720fa37dcfdb8ed8d78
danielhumgon <danielhumgon@gmail.com>
2018-12-26 10:33:20 +0200

    added store

--- src/containers/CustomersContainer.js
+++ src/containers/CustomersContainer.js
@@ -1,8 +1,11 @@
 import React, { Component } from 'react';
+import { withRouter } from 'react-router-dom';
+import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import AppFrame from '../components/AppFrame';
 import CustomersList from '../components/CustomersList';
 import CustomersActions from '../components/CustomersActions';
+import { fetchCustomers } from '../actions/fetchCustomers';
 
 const customers = [
     {
@@ -24,6 +27,15 @@ const customers = [
 ];
 
 class CustomersContainer extends Component {
+
+    componentDidMount() {
+        this.props.fetchCustomers(); 
+    }
+    
+
+    handleAddNew = () => {
+        this.props.history.push('/customers/new');
+    }
 
     renderBody = customers => (
     <div>
@@ -50,7 +62,13 @@ class CustomersContainer extends Component {
 }
 
 CustomersContainer.propTypes = {
-
+    fetchCustomers: PropTypes.func.isRequired,
 };
 
-export default CustomersContainer;
+const mapDispatchToProps = dispatch => (
+    {
+        fetchCustomers: () => dispatch(fetchCustomers())
+    }
+)
+
+export default withRouter(connect(null, mapDispatchToProps) (CustomersContainer));
