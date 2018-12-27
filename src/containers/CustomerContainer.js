0dbc51e92df339196e332f5d5dd22a4e65d7d524
danielhumgon <danielhumgon@gmail.com>
2018-12-27 18:27:57 +0200

    added map state to props

--- src/containers/CustomerContainer.js
+++ src/containers/CustomerContainer.js
@@ -9,7 +9,7 @@ class CustomerContainer extends Component {
             <div>
                 <AppFrame 
                     header={`Customer  ${this.props.dni}`}
-                    body={<p>Customer Data</p>}/>
+                    body={<p>Customer Data "{this.props.customer.name}"</p>}/>
             </div>
         );
     }
@@ -17,6 +17,11 @@ class CustomerContainer extends Component {
 
 CustomerContainer.propTypes = {
     dni: PropTypes.string.isRequired,
+    customer: PropTypes.object.isRequired,
 };
 
-export default connect(null, null)(CustomerContainer);
+const mapStateToProps = (state, props) => ({
+    customer: state.customers.find( c => c.dni === props.dni)
+}); 
+
+export default connect(mapStateToProps, null)(CustomerContainer);
