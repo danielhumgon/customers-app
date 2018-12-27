4a378f719c095b234ef3d10cca3f85047dddc926
danielhumgon <danielhumgon@gmail.com>
2018-12-27 08:57:27 +0200

    api refactor

--- src/actions/fetchCustomers.js
+++ src/actions/fetchCustomers.js
@@ -1,24 +1,6 @@
 import { FETCH_CUSTOMERS } from "../constants";
 import { createAction } from 'redux-actions';
+import { apiGet } from "../api";
+import { urlCustomers } from "../api/urls";
 
-const customers = [
-    {
-        "dni": "12345777",
-        "name": "Kate Austen",
-        "age": 30
-    },
-    {
-        "dni": "15345777",
-        "name": "Hugo Reyes",
-        "age": 32
-    },
-    {
-        "dni": "17345777",
-        "name": "John Locke",
-        "age": 60
-    }
-
-];
-
-
-export const fetchCustomers = createAction(FETCH_CUSTOMERS, () => customers); 
+export const fetchCustomers = createAction(FETCH_CUSTOMERS, apiGet(urlCustomers)); 
