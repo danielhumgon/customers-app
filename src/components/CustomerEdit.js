bea541ca2adda62696f20a4655e5729dddd2972a
danielhumgon <danielhumgon@gmail.com>
2018-12-28 19:11:43 +0200

    added field validations

--- src/components/CustomerEdit.js
+++ src/components/CustomerEdit.js
@@ -4,23 +4,46 @@ import { reduxForm, Field } from 'redux-form';
 import { setPropsAsInitial } from '../helpers/setPropsAsInitial';
 
 
+const isRequired = value => (
+    !value && "This Field is Required"
+);
+
+const isNumber = value => (
+    isNaN(Number(value)) && "The Value Must Be Numeric"
+);
+
+const MyField = ({ input, meta, type, label, name }) => (
+    <div>
+        <label htmlFor={name}>{label}</label>
+        <input {...input} type={!type ? "text" : type}/>
+        {
+           meta.touched && meta.error && <span>{meta.error}</span>
+        }
+    </div>
+)
+
 const CustomerEdit = ({ name, dni, age }) => {
     return (
         <div>
             <h2>Customer Edit</h2>
             <form action="">
-                <div>
-                    <label htmlFor="name">Name</label>
-                    <Field name="name" component="input" type="text"></Field>
-                </div>
-                <div>
-                    <label htmlFor="dni">ID</label>
-                    <Field name="dni" component="input" type="text"></Field>
-                </div>
-                <div>
-                    <label htmlFor="age">Age</label>
-                    <Field name="age" component="input" type="number"></Field>
-                </div>
+
+                    <Field 
+                        name="name" 
+                        component={MyField}
+                        validate={isRequired}
+                        label="Name"></Field>
+                    <Field 
+                        name="dni" 
+                        component={MyField} 
+                        validate={[isRequired, isNumber]}
+                        label="ID"></Field>
+                    <Field 
+                        name="age" 
+                        component={MyField}
+                        type="number"
+                        validate={[isRequired, isNumber]}
+                        label="Age"></Field>   
             </form>
 
         </div>
